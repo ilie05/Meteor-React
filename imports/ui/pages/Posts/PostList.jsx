4e5b23205db26f52e47e1468ca2e1725915d12e9
yoki <petras.moil@gmail.com>
2018-10-13 22:37:52 +0300

    display all post's propeties

--- imports/ui/pages/Posts/PostList.jsx
+++ imports/ui/pages/Posts/PostList.jsx
@@ -27,7 +27,11 @@ export default class PostList extends React.Component {
                         return (
                             <div key={post._id}>
                                 <p>Post id: {post._id} </p>
-                                <p>Post title: {post.title}, Post Description: {post.description} </p>
+                                <p>Post title: {post.title} </p>
+                                <p>Post Description: {post.description} </p>
+                                <p>Post Type: {post.type} </p>
+                                <p>Post Date: {post.createdAt.toString()} </p>
+                                <p>Views: {post.views} </p>
                                 <button onClick={() => {
                                     history.push("/posts/edit/" + post._id)
                                 }}> Edit post
